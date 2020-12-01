2763c3f7dc39bfb79ff0402491f9915eabbd07dc
Errol Cleary <errol.cleary@gmail.com>
2020-12-01 12:02:30 -0800

    Implement bouncing animation

--- src/bouncyDancer.js
+++ src/bouncyDancer.js
@@ -1,6 +1,7 @@
 var makeBouncyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('bouncyDancer');
+  // this.timeBetweenSteps = timeBetweenSteps;
 };
 
 makeBouncyDancer.prototype = Object.create(makeDancer.prototype);
@@ -9,5 +10,15 @@ makeBouncyDancer.prototype.constructor = makeBouncyDancer;
 makeBouncyDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
   // We will need to modify the toggle to something else
-  this.$node.toggle();
+  var position = this.$node.position();
+  if (position.top) {
+    var staticThis = this;
+    this.$node.animate({
+      top: '-=25'
+    }, (staticThis.timeBetweenSteps / 2), function() {
+      staticThis.$node.animate({
+        top: '+=25',
+      }, (staticThis.timeBetweenSteps / 2));
+    });
+  }
 };
